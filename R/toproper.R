f4db315c4c5d84d3495774088cc866d2bb913d05
Jim Leach <jimmy22theave@googlemail.com>
2016-04-11 15:53:13 +0100

    Update toproper.R

--- R/toproper.R
+++ R/toproper.R
@@ -2,7 +2,7 @@
 # the first letter is capitalised, all remaining letters are lower case)
 # N.b. for multi-word strings, only the first word will be affected
 toproper <- function(x) { 
-  first <- touppersubstring(x, 1, 1))
+  first <- toupper(substring(x, 1, 1))
   rest <- tolower(substring(x, 2))
   whole <- paste0(first, rest)
   return(whole)
