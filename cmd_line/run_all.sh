5dea2c11e9ae9d277de2a29078747bb60d13dfa8
Jim Leach <jimmy22theave@googlemail.com>
2018-08-14 17:19:14 -0400

    Update run_all.sh
    Allow a specific folder to be run (move drop script out of this build script), auto-ignore S0 (import scripts), log to a timestamped file.

--- cmd_line/run_all.sh
+++ cmd_line/run_all.sh
@@ -1,5 +1,5 @@
 # Simple script to execute all sql files
-# usage build.sh <username> <schema>
+# usage build.sh <username> <schema> <folder>
 
 # check parameters
 if [ -z "$1" ]; then
@@ -12,27 +12,28 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
+if [ -z "$3" ]; then
+  build_folder="./build"
+else
+  build_folder=$3  
+fi
+
 echo "Running as database user: '$1'"
 echo "Running on schema: '$2'"
+echo "Running folder: '$build_folder'"
 
 #-----------------------------
 
-
-echo Logs in logs/log.txt
+log_date=$(date +"%Y-%m-%d_%H%M%S")
+log_path=logs/"$log_date"-log.txt
+echo SQL logs in $log_path
 
 start_time=$(date +"%y-%d-%m %T")
 
 echo ------------------------------------------
 echo "Building and selecting the schema..."
 
-schema="$2"
-schema_qry="drop schema if exists ${schema} cascade;
-create schema ${schema}; "
-psql --quiet -d [database] -U $1 -h [server] -L logs/log.txt -c "${schema_qry}"
-
-echo "done."
-
-for f in $(find . -name '*.sql' -o -name '*.R' | sort)
+for f in $(find $build_folder -not -path "*/000-s0/*" -name '*.sql' -o -name '*.R' | sort)
 do
 if [[ $f == *".R" ]]
 then
@@ -49,7 +50,7 @@ else
   echo -------------------------------------------
   now=$(date +"%y-%d-%m %T")
   echo "Started: $now"
-  psql --quiet -U $1 -v schema=$schema -v ON_ERROR_STOP=1 -d [database] -h [server] -f $f -L logs/log.txt || exit 1
+  psql --quiet -U $1 -v schema=$schema -v ON_ERROR_STOP=1 -d <database> -h <host> -f $f -L $log_path || exit 1
   now=$(date +"%y-%d-%m %T")
   echo "Finished: $now"
 fi
